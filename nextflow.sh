7b7cc9302c5420428e7a87bb79350b621e1e054f
Valentine Murigneux <v.murigneux@uq.edu.au>
2025-01-23 12:45:21 +1000

    First commit

---
+++ nextflow.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#SBATCH --time=30:00:00
+#SBATCH --job-name=Illumina_LPS_pipeline
+#SBATCH --output=./s%j_job.pipeline_Illumina_L6_ONT_test.out
+#SBATCH --error=./s%j_job.pipeline_Illumina_L6_ONT_test.error
+#SBATCH --account=a_qcif_support
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+module load nextflow/23.04.2
+
+#Directory containing the nextflow.config file and the main.nf script
+dir=/scratch/project_mnt/S0091/valentine/LPS/PIPELINE
+cd ${dir}
+
+#Samplesheet file
+samplesheet=${dir}/samplesheet/samples_L6_test.csv
+
+#Directory that will be created to contain the output files
+out_dir=${dir}/results_test
+
+#Bunya Slurm account 
+slurm_account='a_qcif_support'
+
+#Directory containing the Illumina fastq files
+fqdir=${dir}/fastq
+#cp /QRISdata/Q2313/AGRF-raw-Illumina/AGRF_CAGRF24030272-2_22VH7WLT3/*fastq.gz ${fqdir}
+
+#Run the pipeline (-resume is used to restart the pipeline if it fails)
+nextflow main.nf --outdir ${out_dir} --fqdir ${fqdir} --samplesheet ${samplesheet} -resume
+
+
